e58332e56dbc3b2266eb768b648131ac730d221a
jasonz001 <jasonz001@outlook.com>
2015-10-27 18:45:42 -0700

    Added Menu functionality and fixed compilation errors
    + Game Manager controls all the states and menus
    + Each state represents a menu
    
    TODO: move the AR menu to be activated by a button

--- Assets/GameState/SharedModeMenuState.cs
+++ Assets/GameState/SharedModeMenuState.cs
@@ -1,7 +1,28 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class SharedModeMenuState : State {
 
+    // Insert UI elements here
+    //
+    //
+
+    // To use the List class, use the System.Collections.Generic library
+    public List<Player> planters;
+    public List<Player> defusers;
+    public int numberOfBombs;
+    public float timeToPlant;
+
+    public void Initialize()
+    {
+        //Reset the UI here
+    }
+
+    public void PlantBomb()
+    {
+        gameManager.plantBombState.Initialize(planters, defusers, numberOfBombs, timeToPlant);
+        gameManager.SetState(gameManager.plantBombState);
+    }
 
 }
