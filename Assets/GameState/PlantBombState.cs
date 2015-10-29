a83c0bdd25845e21a55d3a26eb9c0f05dfa957a0
srsuttiratana <sarah_suttiratana@hotmail.com>
2015-10-28 18:12:34 -0700

    Added PlantBombState

--- Assets/GameState/PlantBombState.cs
+++ Assets/GameState/PlantBombState.cs
@@ -1,20 +1,44 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
 public class PlantBombState : State {
-
-    /* Initialize the plant bomb state
-     * Pass in the planters, defusers and number of bombs
-     */
-
-    List<Player> planters;
-    List<Player> defusers;
-    int numOfBombs;
-    float timeToPlant;
-
-    public void Initialize(List<Player> planters, List<Player> defusers, int numOfBombs, float timeToPlant)
+    
+    public override void ToTutorialMenu()
     {
+        //gameManager.SetState(gameManager.mainMenuState);
+    }
 
+    public override void ToMultiplayerMenu()
+    {
+        //gameManager.SetState(gameManager.MultiplayerMenuState);
+    }
+
+    public override void PlantBomb()
+    {
+        //gameManager.SetState(gameManager.PlantBombState);
+    }
+
+    // Changes the game state between planting bomb and defusing bomb
+    public override void PassPhone(Player from, Player to)
+    {
+        //do nothing
+    }
+
+
+    // Time runs out
+    public override void TimeExpired()
+    {
+        //do nothing
+    }
+
+    // All bombs are defused
+    public override void AllBombsDefused()
+    {
+        //do nothing
+    }
+
+    public void execute()
+    {
+        SharedModeMenuState.timeAllowed
     }
 }
